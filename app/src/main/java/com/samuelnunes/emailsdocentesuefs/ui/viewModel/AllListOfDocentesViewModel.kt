cb2d00c6357f1bb0646810e68057e77c5b5a2510
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2021-04-16 12:04:55 -0300

    Add GitModules

---
+++ app/src/main/java/com/samuelnunes/emailsdocentesuefs/ui/viewModel/AllListOfDocentesViewModel.kt
@@ -0,0 +1,7 @@
+package com.samuelnunes.emailsdocentesuefs.ui.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class AllListOfDocentesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
